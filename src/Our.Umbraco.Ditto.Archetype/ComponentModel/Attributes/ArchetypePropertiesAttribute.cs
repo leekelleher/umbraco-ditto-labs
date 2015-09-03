104b1204420bdc7d5da759d36dc18927cde9dd72
leekelleher <leekelleher@gmail.com>
2015-09-03 21:03:35 +0100

    Fleshed out the ArchetypePropertyValueResolver (following Ditto's UmbracoProperty attribute)

---
+++ src/Our.Umbraco.Ditto.Archetype/ComponentModel/Attributes/ArchetypePropertiesAttribute.cs
@@ -0,0 +1,10 @@
+﻿namespace Our.Umbraco.Ditto.Archetype
+{
+    using System;
+
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public class ArchetypePropertiesAttribute: Attribute
+    {
+        public string Prefix { get; set; }
+    }
+}
--- src/Our.Umbraco.Ditto.Archetype/ComponentModel/ValueResolvers/ArchetypePropertyValueResolver.cs
+++ src/Our.Umbraco.Ditto.Archetype/ComponentModel/ValueResolvers/ArchetypePropertyValueResolver.cs
@@ -1,15 +1,70 @@
-﻿using System;
-
-namespace Our.Umbraco.Ditto.Archetype
+﻿namespace Our.Umbraco.Ditto.Archetype
 {
+    using System.Reflection;
+    using global::Umbraco.Core;
+
     public class ArchetypePropertyValueResolver : DittoArchetypeValueResolver<DittoArchetypeValueResolverContext, ArchetypePropertyAttribute>
     {
         public override object ResolveValue()
         {
-            var propertyAlias = this.Context.PropertyDescriptor != null ? this.Context.PropertyDescriptor.Name : string.Empty;
-            var archetypePropertyAlias = this.Attribute.PropertyName ?? propertyAlias;
+            var defaultValue = Attribute.DefaultValue;
+            var propName = Context.PropertyDescriptor != null ? Context.PropertyDescriptor.Name : string.Empty;
+            var altPropName = string.Empty;
 
-            return this.Fieldset.GetValue(archetypePropertyAlias);
+            // Check for Archetype properties attribute on class
+            if (Context.PropertyDescriptor != null)
+            {
+                var classAttr = Context.PropertyDescriptor.ComponentType
+                    .GetCustomAttribute<ArchetypePropertiesAttribute>();
+                if (classAttr != null)
+                {
+                    // Apply the prefix
+                    if (!string.IsNullOrWhiteSpace(classAttr.Prefix))
+                    {
+                        altPropName = propName;
+                        propName = classAttr.Prefix + propName;
+                    }
+                }
+            }
+
+            var archetypePropertyName = Attribute.PropertyName ?? propName;
+            var altArchetypePropertyName = Attribute.AltPropertyName ?? altPropName;
+
+            if (this.Fieldset == null)
+            {
+                return defaultValue;
+            }
+
+            var contentType = this.Fieldset.GetType();
+            object propertyValue = null;
+
+            // Try fetching the value.
+            if (!archetypePropertyName.IsNullOrWhiteSpace())
+            {
+                var contentProperty = contentType.GetProperty(archetypePropertyName, BindingFlags.Public | BindingFlags.IgnoreCase | BindingFlags.Instance | BindingFlags.Static);
+                propertyValue = contentProperty != null
+                    ? contentProperty.GetValue(this.Fieldset, null)
+                    : this.Fieldset.GetValue(archetypePropertyName);
+            }
+
+            // Try fetching the alt value.
+            if ((propertyValue == null || propertyValue.ToString().IsNullOrWhiteSpace())
+                && !string.IsNullOrWhiteSpace(altArchetypePropertyName))
+            {
+                var contentProperty = contentType.GetProperty(altArchetypePropertyName, BindingFlags.Public | BindingFlags.IgnoreCase | BindingFlags.Instance | BindingFlags.Static);
+                propertyValue = contentProperty != null
+                    ? contentProperty.GetValue(this.Fieldset, null)
+                    : this.Fieldset.GetValue(altArchetypePropertyName);
+            }
+
+            // Try setting the default value.
+            if ((propertyValue == null || propertyValue.ToString().IsNullOrWhiteSpace())
+                && defaultValue != null)
+            {
+                propertyValue = defaultValue;
+            }
+
+            return propertyValue;
         }
     }
 }
