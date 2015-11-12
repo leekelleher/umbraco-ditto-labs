ae7d2b34cc22b5bc1e8ed16e2000c7066cb488a2
leekelleher <leekelleher@gmail.com>
2015-11-12 21:14:13 +0000

    [WIP] Relations TypeConverter + unit-test.
    (Very much a work-in-progress!)

---
+++ src/Our.Umbraco.Ditto.Contrib/ComponentModel/TypeConverters/UmbracoRelationConverter.cs
@@ -0,0 +1,44 @@
+﻿namespace Our.Umbraco.Ditto
+{
+	using System;
+	using System.ComponentModel;
+	using System.Globalization;
+	using global::Umbraco.Core.Models;
+
+	public class UmbracoRelationConverter : DittoConverter
+	{
+		public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+		{
+			return typeof(IRelation).IsAssignableFrom(sourceType);
+		}
+
+		public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType)
+		{
+			return typeof(IPublishedContent).IsAssignableFrom(destinationType);
+		}
+
+		public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+		{
+			if (context == null || context.PropertyDescriptor == null || value == null)
+			{
+				// There's no way to determine the type here.
+				return null;
+			}
+
+			var propertyType = context.PropertyDescriptor.PropertyType;
+			//var isGenericType = propertyType.IsGenericType;
+			//var targetType = isGenericType
+			//	? propertyType.GenericTypeArguments.First()
+			//	: propertyType;
+
+			if (value is IRelation)
+			{
+				var relation = (IRelation)value;
+
+				return this.ConvertContentFromInt(relation.ChildId, propertyType, culture);
+			}
+
+			return base.ConvertFrom(context, culture, value);
+		}
+	}
+}
