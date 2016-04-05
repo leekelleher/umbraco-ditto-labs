b4aac507dd0fec6ba5e917b4611cfb30d499c8f3
leekelleher <leekelleher@gmail.com>
2016-04-05 16:33:52 +0100

    Archetype: Fixed the nested Archetype unit-test

--- tests/Our.Umbraco.Ditto.Archetype.Tests/NestedArchetypeMappingTests.cs
+++ tests/Our.Umbraco.Ditto.Archetype.Tests/NestedArchetypeMappingTests.cs
@@ -1,5 +1,6 @@
 ﻿namespace Our.Umbraco.Ditto.Archetype.Tests
 {
+    using System.Collections.Generic;
     using System.Linq;
     using global::Archetype.Models;
     using global::Archetype.PropertyConverters;
@@ -9,14 +10,21 @@
     public class NestedArchetypeMappingTests
     {
         private ArchetypeModel _archetype;
+        private ArchetypeModel _nestedArchetype;
 
         [TestFixtureSetUp]
         public void Init()
         {
-            var archetypeJson = "{\"fieldsets\":[{\"properties\":[{\"alias\":\"name\",\"value\":\"Section 1\"},{\"alias\":\"content\",\"value\":\"{\\\"fieldsets\\\":[{\\\"properties\\\":[{\\\"alias\\\":\\\"contentType\\\",\\\"value\\\":\\\"Static\\\"}],\\\"alias\\\":\\\"section\\\",\\\"disabled\\\":false},{\\\"properties\\\":[{\\\"alias\\\":\\\"contentType\\\",\\\"value\\\":\\\"Dynamic\\\"}],\\\"alias\\\":\\\"section\\\",\\\"disabled\\\":false},{\\\"properties\\\":[{\\\"alias\\\":\\\"contentType\\\",\\\"value\\\":\\\"Branded\\\"}],\\\"alias\\\":\\\"section\\\",\\\"disabled\\\":false}]}\"}],\"alias\":\"sections\",\"disabled\":false}]}";
             var converter = new ArchetypeValueConverter();
 
+            var archetypeJson = "{\"fieldsets\":[{\"properties\":[{\"alias\":\"name\",\"value\":\"Section 1\"},{\"alias\":\"content\",\"value\":\"\"}],\"alias\":\"sections\",\"disabled\":false}]}";
             _archetype = (ArchetypeModel)converter.ConvertDataToSource(null, archetypeJson, false);
+
+            // HACK: [LK] Due to internal workings of Archetype, it is difficult to contruct a nested Archetype model
+            // from a serialized JSON string. We have to revert to taking a manual ceremony.
+            var nestedArchetypeJson = "{\"fieldsets\":[{\"properties\":[{\"alias\":\"contentType\",\"value\":\"Static\"}],\"alias\":\"section\",\"disabled\":false},{\"properties\":[{\"alias\":\"contentType\",\"value\":\"Dynamic\"}],\"alias\":\"section\",\"disabled\":false},{\"properties\":[{\"alias\":\"contentType\",\"value\":\"Branded\"}],\"alias\":\"section\",\"disabled\":false}]}";
+            _nestedArchetype = (ArchetypeModel)converter.ConvertDataToSource(null, nestedArchetypeJson, false);
+            _archetype.First().Properties.First(x => x.Alias == "content").Value = _nestedArchetype;
         }
 
         [Test]
@@ -30,7 +38,13 @@
         {
             public string Name { get; set; }
 
-            public ArchetypeModel Content { get; set; }
+            [DittoArchetype]
+            public IEnumerable<MyNestedModel> Content { get; set; }
+        }
+
+        public class MyNestedModel
+        {
+            public string ContentType { get; set; }
         }
 
         [Test]
@@ -43,8 +57,11 @@
             Assert.IsInstanceOf<MyModel>(model);
             Assert.That(model.Name, Is.EqualTo("Section 1"));
 
-            // TODO: [LK] Need to work on this part, how to convert Archetype's JSON string into a strongly-typed `ArchetypeModel`?
-            Assert.IsNull(model.Content);
+            Assert.IsNotNull(model.Content);
+            Assert.IsInstanceOf<IEnumerable<MyNestedModel>>(model.Content);
+
+            var nestedFirst = model.Content.First();
+            Assert.That(nestedFirst.ContentType, Is.EqualTo("Static"));
         }
     }
 }
