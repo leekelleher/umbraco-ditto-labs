328275fe35493ab412af031b3a6ba44bf79fd9a9
leekelleher <leekelleher@gmail.com>
2016-08-18 17:30:17 +0100

    Contrib: added AggregateValues processor
    This processor attempts to aggregate the recursive values of a property, storing them as a list/enumerable.

---
+++ src/Our.Umbraco.Ditto.Contrib/Aggregation/AggregateValues.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Umbraco.Core;
+using Umbraco.Core.Models;
+using Umbraco.Web;
+
+namespace Our.Umbraco.Ditto
+{
+    public class AggregateValues : DittoProcessorAttribute
+    {
+        public AggregateValues()
+            : this(null)
+        { }
+
+        public AggregateValues(string propertyName)
+            : this(propertyName, false)
+        { }
+
+        public AggregateValues(string propertyName, bool reverseOrder)
+        {
+            this.PropertyName = propertyName;
+            this.ReverseOrder = reverseOrder;
+        }
+
+        public string PropertyName { get; set; }
+
+        public bool ReverseOrder { get; set; }
+
+        public override object ProcessValue()
+        {
+            var targetType = this.Context.PropertyDescriptor.PropertyType;
+
+            if (targetType == this.Value.GetType())
+                return this.Value;
+
+            var content = this.Value is IPublishedContent
+                ? this.Value as IPublishedContent
+                : this.Context.Content;
+
+            var propName = this.Context.PropertyDescriptor != null ? this.Context.PropertyDescriptor.Name : string.Empty;
+            var umbracoPropertyName = this.PropertyName ?? propName;
+
+            var innerType = targetType.IsGenericType
+                ? targetType.GenericTypeArguments.First()
+                : targetType;
+
+            var listType = typeof(List<>).MakeGenericType(innerType);
+
+            var items = (IList)Activator.CreateInstance(listType);
+
+            while (content != null)
+            {
+                if (content.HasValue(umbracoPropertyName))
+                {
+                    // TODO: [LK:2016-08-13] Ask MB if it's possible to reuse `UmbracoPropertyAttribute` here?
+
+                    var propValue = content.GetPropertyValue(umbracoPropertyName);
+                    var attempt = propValue.TryConvertTo(innerType);
+
+                    if (attempt.Success && attempt.Result != null)
+                    {
+                        if (ReverseOrder)
+                        {
+                            items.Insert(0, attempt.Result);
+                        }
+                        else
+                        {
+                            items.Add(attempt.Result);
+                        }
+                    }
+                }
+
+                content = content.Parent;
+            }
+
+            return items;
+        }
+    }
+}
---
+++ tests/Our.Umbraco.Ditto.Contrib.Tests/AggregateValuesTests.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Our.Umbraco.Ditto.Contrib.Tests.Mocks;
+
+namespace Our.Umbraco.Ditto.Contrib
+{
+    [TestFixture]
+    public class AggregateValuesTests
+    {
+        const string PropertyNameAlias = "myProperty";
+
+        public class MyModel
+        {
+            [AggregateValues]
+            public IEnumerable<string> MyProperty { get; set; }
+
+            [AggregateValues(PropertyName = PropertyNameAlias, ReverseOrder = true)]
+            public IEnumerable<string> MyPropertyReversed { get; set; }
+        }
+
+        private MockPublishedContent Content;
+
+        private List<string> Values;
+
+        [TestFixtureSetUp]
+        public void Init()
+        {
+            /*
+             * This test will attempt to aggregate the recursive values of a property.
+             * Taking the value from the current content node, then moving upwards through the content tree.
+             * The unit-test is to check that the values set-up here are mapped accordingly in the Ditto processor.
+             * An additional option of `ReverseOrder` is used to reverse the order of the list values.
+             */
+
+            this.Values = new List<string>() { "hello", "world", "foo" };
+
+            var level1 = new MockPublishedContent
+            {
+                Properties = new[] { new MockPublishedContentProperty(PropertyNameAlias, this.Values[0], true) }
+            };
+
+            var level2 = new MockPublishedContent
+            {
+                Parent = level1,
+                Properties = new[] { new MockPublishedContentProperty(PropertyNameAlias, this.Values[1], true) }
+            };
+
+            var level3 = new MockPublishedContent
+            {
+                Parent = level2,
+                Properties = new[] { new MockPublishedContentProperty(PropertyNameAlias, this.Values[2], true) }
+            };
+
+            this.Content = level3;
+        }
+
+        [Test]
+        public void AggregateValues_Test()
+        {
+            var model = this.Content.As<MyModel>();
+
+            Assert.That(model, Is.Not.Null);
+            Assert.That(model.MyProperty, Is.Not.Null);
+            Assert.That(model.MyPropertyReversed, Is.Not.Null);
+
+            // Make the value as a list, so it can be evaluated.
+            var list = model.MyProperty.ToList();
+
+            Assert.That(list, Has.Count);
+            Assert.That(list.Count, Is.EqualTo(this.Values.Count));
+
+            // The processor collects the values from the bottom-up,
+            // they will be in the opposite order to our initial values,
+            // so they will not be "equal", but they are "equivalent".
+            CollectionAssert.AreNotEqual(list, this.Values);
+            CollectionAssert.AreEquivalent(list, this.Values);
+
+            // Make the value as a list, so it can be evaluated.
+            var reversed = model.MyPropertyReversed.ToList();
+
+            Assert.That(reversed, Has.Count);
+            Assert.That(reversed.Count, Is.EqualTo(this.Values.Count));
+
+            // The reversed order of the values should match the order of our initial values.
+            CollectionAssert.AreEqual(reversed, this.Values);
+        }
+    }
+}
